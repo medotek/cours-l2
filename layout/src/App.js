00a8d514ae9180c30d6db367f80189326877f207
Ubuntu <ubuntu@ip-172-31-35-176.ec2.internal>
2020-02-05 14:50:37 +0000

    maj5

--- layout/src/App.js
+++ layout/src/App.js
@@ -6,16 +6,23 @@ import './App.css';
 
 
 class App extends React.Component {
+  static defaultProps = {
+    name: data['results'][0].name['title'] + ' ' + data['results'][0].name['first'] + ' ' + data['results'][0].name['last'],
+    mdp: data['results'][0].login['password'],
+    username: data['results'][0].login['username'],
+    img: data['results'][0].picture['thumbnail']
+    
+  } 
   render() {
   return (
     <div className="App">
     <div class="container">
       <div className="card ml-0 mr-0 m-auto data">
-        <img className="card-img-top img-fluid" src={data['results'][0].picture['thumbnail']}  alt="Card image cap" />
+        <img className="card-img-top img-fluid" src={this.props.img}  alt="Card image cap" />
         <div className="card-body">
-          <h5 className="card-title">{data['results'][0].name['title']} {data['results'][0].name['first']} {data['results'][0].name['last']} </h5>
-          <p className="card-text">Nom d'utilisateur : {data['results'][0].login['username']}</p>
-          <p clas="card-text">Mot de passe : {data['results'][0].login['password']}</p>
+          <h5 className="card-title">{this.props.name}</h5>
+          <p className="card-text">Nom d'utilisateur : {this.props.username}</p>
+          <p clas="card-text">Mot de passe : {this.props.mdp}</p>
           <a href="https://tinder.com" className="btn btn-primary">Accéder à son profil</a>
         </div>
       </div>
