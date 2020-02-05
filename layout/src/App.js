9d3b44fc8e044f7f0160b20796323c730091d9dc
Ubuntu <ubuntu@ip-172-31-35-176.ec2.internal>
2020-02-05 14:30:12 +0000

    maj3

--- layout/src/App.js
+++ layout/src/App.js
@@ -10,13 +10,13 @@ class App extends React.Component {
   return (
     <div className="App">
     <div class="container">
-      <div class="card ml-0 mr-0 m-auto data">
-        <img class="card-img-top img-fluid" src={data['results'][0].picture['thumbnail']}  alt="Card image cap" />
-        <div class="card-body">
-          <h5 class="card-title">{data['results'][0].name['title']} {data['results'][0].name['first']} {data['results'][0].name['last']} </h5>
-          <p class="card-text">Nom d'utilisateur : {data['results'][0].login['username']}</p>
+      <div className="card ml-0 mr-0 m-auto data">
+        <img className="card-img-top img-fluid" src={data['results'][0].picture['thumbnail']}  alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">{data['results'][0].name['title']} {data['results'][0].name['first']} {data['results'][0].name['last']} </h5>
+          <p className="card-text">Nom d'utilisateur : {data['results'][0].login['username']}</p>
           <p clas="card-text">Mot de passe : {data['results'][0].login['password']}</p>
-          <a href="https://tinder.com" class="btn btn-primary">Accéder à son profil</a>
+          <a href="https://tinder.com" className="btn btn-primary">Accéder à son profil</a>
         </div>
       </div>
       </div>
