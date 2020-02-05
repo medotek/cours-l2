2c20c5cc9f886bbc3d164af8c7391b29215c6160
medotek <tomsyan03@gmail.com>
2020-02-05 07:27:37 +0000

    1er push

--- timer/src/App.js
+++ timer/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
-import Map from "./components/map/Map";
 import './App.css';
-
+import Timer from './components/timer/Timer.js';
 function App() {
   return (
     <div className="App">
-	  <Map center={ { lat: 45.1885, lng: 5.7245} } />
+        <Timer />
     </div>
   );
 }
--- timer/src/components/timer/Timer.js
+++ timer/src/components/timer/Timer.js
@@ -4,24 +4,43 @@ class Timer extends React.Component {
 
   constructor(props) {
     super(props);
-    // ...
+    this.state = { seconds : 0 };
+    this.s = "s";
   }
 
   componentDidMount() {
-    // ...
+    this.interval = setInterval(
+      () => this.counter(),
+      1000
+    );
   }
 
   componentWillUnmount() {
-    // ...
+    clearInterval(this.timerID);
   }
   
+  counter() {
+    this.setState( state => ({
+      seconds: state.seconds + 1
+    }));
+  }
+  
+  
+  
   render () {
+    if (this.state.seconds >= 2) {
+      this.s = "s";
+    } else {
+      this.s = "";      
+    }
     return (
       <div data-testid="timer">
-        {this.state.counter}
+          <h2>{this.state.seconds}  Hindi{this.s} </h2>
       </div>
     );
   }
+  
+  
 }
 
 export default Timer;
