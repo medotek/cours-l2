d90a85df29179026a9921a3805ab0c2a8703cc71
Ubuntu <ubuntu@ip-172-31-35-176.ec2.internal>
2020-02-08 13:35:24 +0000

    maj12

--- layout/src/App.js
+++ layout/src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import Layout from "./components/layout.js";
 import './App.css';
 
 
 class App extends React.Component {
  
-  
-  constructor(props) {
-    super(props)
-    
-  }
-  
+
   render() {
   return (
     <div className="App">
